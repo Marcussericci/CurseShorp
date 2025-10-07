04e324064f0caec815c214f8d9016616d96169bc
Kirill185 <Bogatovkirill799@gmail.com>
2025-10-07 13:09:39 +0500

    Добавьте файлы проекта.

---
+++ asdasd/class/BaseClass/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdasd.@class.BaseClass
+{
+    public class Class1
+    {
+        private int Id;
+        internal string Name;
+        protected string Model;
+        public DateTime CreateDate;
+        internal protected DateTime UseDate;
+
+    }
+}
