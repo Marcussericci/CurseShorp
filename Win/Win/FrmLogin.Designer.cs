5e8d81f4da4c49150f01dfd6192a4cb0a4642dd7
Marcussericci <maksimfyvska@gmail.com>
2025-11-11 11:13:08 +0500

    Add files via upload

---
+++ Win/Win/FrmLogin.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Win
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txtname = new TextBox();
+            txtpass = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(105, 75);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Close";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 75);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 2;
+            label1.Text = "UserName";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 39);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Password";
+            label2.Click += label2_Click;
+            // 
+            // txtname
+            // 
+            txtname.Location = new Point(80, 6);
+            txtname.Name = "txtname";
+            txtname.Size = new Size(100, 23);
+            txtname.TabIndex = 4;
+            // 
+            // txtpass
+            // 
+            txtpass.Location = new Point(80, 35);
+            txtpass.Name = "txtpass";
+            txtpass.PasswordChar = '*';
+            txtpass.Size = new Size(100, 23);
+            txtpass.TabIndex = 5;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(199, 106);
+            Controls.Add(txtpass);
+            Controls.Add(txtname);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "FrmLogin";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private TextBox txtname;
+        private TextBox txtpass;
+    }
+}
---
+++ Win/Win/FrmLogin.cs
@@ -0,0 +1,37 @@
+namespace Win
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtname.Text == "asd" && txtpass.Text == "123")
+            {
+
+                main main = new main();
+                this.Hide();
+                main.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("You need to write correct Username and password");
+            }
+        }
+
+ 
+    }
+}
